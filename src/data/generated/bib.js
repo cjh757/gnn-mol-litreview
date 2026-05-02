7cb54d025ac7d6494bece8969f40aa1a41e5a9ed
cjh757 <15890059072@163.com>
2026-05-02 23:46:15 +0800

    Update bib.js

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,98 @@
-﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
-        "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
-    }
-};
+var bibtex = `
+@article{Gilmer2017,
+  title={Neural message passing for quantum chemistry},
+  author={Gilmer, Justin and others},
+  journal={ICML},
+  year={2017},
+  doi={10.48550/arXiv.1704.01212},
+  keywords={GNN Framework}
+}
+@article{Schutt2018,
+  title={SchNet – A deep learning architecture for molecules and materials},
+  author={Schütt, K. T. and others},
+  journal={The Journal of Chemical Physics},
+  year={2018},
+  doi={10.1063/1.5019779},
+  keywords={Distance-based GNN}
+}
+@article{Gasteiger2020,
+  title={Directional message passing for molecular graphs},
+  author={Gasteiger, Johannes and others},
+  journal={ICLR},
+  year={2020},
+  doi={10.48550/arXiv.2003.03123},
+  keywords={Directional GNN}
+}
+@article{Ramakrishnan2014,
+  title={Quantum chemistry structures and properties of 134 kilo molecules},
+  author={Ramakrishnan, Raghunathan and others},
+  journal={Scientific Data},
+  year={2014},
+  doi={10.1038/sdata.2014.22},
+  keywords={Benchmark Dataset}
+}
+@article{Chmiela2017,
+  title={Machine learning of accurate energy-conserving molecular force fields},
+  author={Chmiela, Stefan and others},
+  journal={Science Advances},
+  year={2017},
+  doi={10.1126/sciadv.1603015},
+  keywords={Benchmark Dataset}
+}
+@article{Liu2022,
+  title={Spherical message passing for 3D molecular graphs},
+  author={Liu, Yi and others},
+  journal={ICLR},
+  year={2022},
+  doi={10.48550/arXiv.2102.05013},
+  keywords={Directional GNN}
+}
+@article{Xu2019,
+  title={How powerful are graph neural networks?},
+  author={Xu, Keyulu and others},
+  journal={ICLR},
+  year={2019},
+  doi={10.48550/arXiv.1810.00826},
+  keywords={GNN Theory}
+}
+@article{Hu2020,
+  title={Strategies for pre-training graph neural networks},
+  author={Hu, Weihua and others},
+  journal={ICLR},
+  year={2020},
+  doi={10.48550/arXiv.1905.12265},
+  keywords={Pre-training GNNs}
+}
+@article{Schutt2021,
+  title={Equivariant message passing for the prediction of tensorial properties and molecular spectra},
+  author={Schütt, K. T. and others},
+  journal={ICML},
+  year={2021},
+  doi={10.48550/arXiv.2102.03150},
+  keywords={Equivariant GNN}
+}
+@article{Simeon2023,
+  title={TensorNet: Cartesian tensor representations for efficient learning of molecular potentials},
+  author={Simeon, Guillem and De Fabritiis, Gianni},
+  journal={NeurIPS},
+  year={2023},
+  doi={10.48550/arXiv.2306.06482},
+  keywords={Equivariant GNN}
+}
+@article{Wang2024,
+  title={Enhancing geometric representations for molecules with equivariant vector-scalar interactive message passing},
+  author={Wang, Y. and others},
+  journal={Nature Communications},
+  year={2024},
+  doi={10.1038/s41467-023-43720-2},
+  keywords={Equivariant GNN}
+}
+@article{See2024,
+  title={Graph neural network-based molecular property prediction with patch aggregation},
+  author={See, T. J. and others},
+  journal={Journal of Chemical Theory and Computation},
+  year={2024},
+  doi={10.1021/acs.jctc.4c00798},
+  keywords={Aggregation Mechanism}
+}
+`;
