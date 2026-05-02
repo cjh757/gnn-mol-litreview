3c159b3d35db8823b45129739b01ab85420f624e
cjh757 <15890059072@163.com>
2026-05-03 01:01:54 +0800

    Add files via upload

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,14 +1,110 @@
-var entries = [
-  {"type":"article","id":"Gilmer2017","title":"Neural message passing for quantum chemistry","author":"Gilmer, Justin and others","journal":"ICML","year":"2017","doi":"10.48550/arXiv.1704.01212","keywords":"GNN Framework"},
-  {"type":"article","id":"Schutt2018","title":"SchNet – A deep learning architecture for molecules and materials","author":"Schütt, K. T. and others","journal":"The Journal of Chemical Physics","year":"2018","doi":"10.1063/1.5019779","keywords":"Distance-based GNN"},
-  {"type":"article","id":"Gasteiger2020","title":"Directional message passing for molecular graphs","author":"Gasteiger, Johannes and others","journal":"ICLR","year":"2020","doi":"10.48550/arXiv.2003.03123","keywords":"Directional GNN"},
-  {"type":"article","id":"Ramakrishnan2014","title":"Quantum chemistry structures and properties of 134 kilo molecules","author":"Ramakrishnan, Raghunathan and others","journal":"Scientific Data","year":"2014","doi":"10.1038/sdata.2014.22","keywords":"Benchmark Dataset"},
-  {"type":"article","id":"Chmiela2017","title":"Machine learning of accurate energy-conserving molecular force fields","author":"Chmiela, Stefan and others","journal":"Science Advances","year":"2017","doi":"10.1126/sciadv.1603015","keywords":"Benchmark Dataset"},
-  {"type":"article","id":"Liu2022","title":"Spherical message passing for 3D molecular graphs","author":"Liu, Yi and others","journal":"ICLR","year":"2022","doi":"10.48550/arXiv.2102.05013","keywords":"Directional GNN"},
-  {"type":"article","id":"Xu2019","title":"How powerful are graph neural networks?","author":"Xu, Keyulu and others","journal":"ICLR","year":"2019","doi":"10.48550/arXiv.1810.00826","keywords":"GNN Theory"},
-  {"type":"article","id":"Hu2020","title":"Strategies for pre-training graph neural networks","author":"Hu, Weihua and others","journal":"ICLR","year":"2020","doi":"10.48550/arXiv.1905.12265","keywords":"Pre-training GNNs"},
-  {"type":"article","id":"Schutt2021","title":"Equivariant message passing for the prediction of tensorial properties and molecular spectra","author":"Schütt, K. T. and others","journal":"ICML","year":"2021","doi":"10.48550/arXiv.2102.03150","keywords":"Equivariant GNN"},
-  {"type":"article","id":"Simeon2023","title":"TensorNet: Cartesian tensor representations for efficient learning of molecular potentials","author":"Simeon, Guillem and De Fabritiis, Gianni","journal":"NeurIPS","year":"2023","doi":"10.48550/arXiv.2306.06482","keywords":"Equivariant GNN"},
-  {"type":"article","id":"Wang2024","title":"Enhancing geometric representations for molecules with equivariant vector-scalar interactive message passing","author":"Wang, Y. and others","journal":"Nature Communications","year":"2024","doi":"10.1038/s41467-023-43720-2","keywords":"Equivariant GNN"},
-  {"type":"article","id":"See2024","title":"Graph neural network-based molecular property prediction with patch aggregation","author":"See, T. J. and others","journal":"Journal of Chemical Theory and Computation","year":"2024","doi":"10.1021/acs.jctc.4c00798","keywords":"Aggregation Mechanism"}
-];
+﻿const generatedBibEntries = {
+    "Chmiela2017": {
+        "author": "Chmiela, Stefan and others",
+        "doi": "10.1126/sciadv.1603015",
+        "journal": "Science Advances",
+        "keywords": "Benchmark Dataset",
+        "title": "Machine learning of accurate energy-conserving molecular force fields",
+        "type": "article",
+        "year": "2017"
+    },
+    "Gasteiger2020": {
+        "author": "Gasteiger, Johannes and others",
+        "doi": "10.48550/arXiv.2003.03123",
+        "journal": "ICLR",
+        "keywords": "Directional GNN",
+        "title": "Directional message passing for molecular graphs",
+        "type": "article",
+        "year": "2020"
+    },
+    "Gilmer2017": {
+        "author": "Gilmer, Justin and others",
+        "doi": "10.48550/arXiv.1704.01212",
+        "journal": "ICML",
+        "keywords": "GNN Framework",
+        "title": "Neural message passing for quantum chemistry",
+        "type": "article",
+        "year": "2017"
+    },
+    "Hu2020": {
+        "author": "Hu, Weihua and others",
+        "doi": "10.48550/arXiv.1905.12265",
+        "journal": "ICLR",
+        "keywords": "Pre-training GNNs",
+        "title": "Strategies for pre-training graph neural networks",
+        "type": "article",
+        "year": "2020"
+    },
+    "Liu2022": {
+        "author": "Liu, Yi and others",
+        "doi": "10.48550/arXiv.2102.05013",
+        "journal": "ICLR",
+        "keywords": "Directional GNN",
+        "title": "Spherical message passing for 3D molecular graphs",
+        "type": "article",
+        "year": "2022"
+    },
+    "Ramakrishnan2014": {
+        "author": "Ramakrishnan, Raghunathan and others",
+        "doi": "10.1038/sdata.2014.22",
+        "journal": "Scientific Data",
+        "keywords": "Benchmark Dataset",
+        "title": "Quantum chemistry structures and properties of 134 kilo molecules",
+        "type": "article",
+        "year": "2014"
+    },
+    "Schutt2018": {
+        "author": "Sch\u00fctt, K. T. and others",
+        "doi": "10.1063/1.5019779",
+        "journal": "The Journal of Chemical Physics",
+        "keywords": "Distance-based GNN",
+        "title": "SchNet \u2013 A deep learning architecture for molecules and materials",
+        "type": "article",
+        "year": "2018"
+    },
+    "Schutt2021": {
+        "author": "Sch\u00fctt, K. T. and others",
+        "doi": "10.48550/arXiv.2102.03150",
+        "journal": "ICML",
+        "keywords": "Equivariant GNN",
+        "title": "Equivariant message passing for the prediction of tensorial properties and molecular spectra",
+        "type": "article",
+        "year": "2021"
+    },
+    "See2024": {
+        "author": "See, T. J. and others",
+        "doi": "10.1021/acs.jctc.4c00798",
+        "journal": "Journal of Chemical Theory and Computation",
+        "keywords": "Aggregation Mechanism",
+        "title": "Graph neural network-based molecular property prediction with patch aggregation",
+        "type": "article",
+        "year": "2024"
+    },
+    "Simeon2023": {
+        "author": "Simeon, Guillem and De Fabritiis, Gianni",
+        "doi": "10.48550/arXiv.2306.06482",
+        "journal": "NeurIPS",
+        "keywords": "Equivariant GNN",
+        "title": "TensorNet: Cartesian tensor representations for efficient learning of molecular potentials",
+        "type": "article",
+        "year": "2023"
+    },
+    "Wang2024": {
+        "author": "Wang, Y. and others",
+        "doi": "10.1038/s41467-023-43720-2",
+        "journal": "Nature Communications",
+        "keywords": "Equivariant GNN",
+        "title": "Enhancing geometric representations for molecules with equivariant vector-scalar interactive message passing",
+        "type": "article",
+        "year": "2024"
+    },
+    "Xu2019": {
+        "author": "Xu, Keyulu and others",
+        "doi": "10.48550/arXiv.1810.00826",
+        "journal": "ICLR",
+        "keywords": "GNN Theory",
+        "title": "How powerful are graph neural networks?",
+        "type": "article",
+        "year": "2019"
+    }
+};
